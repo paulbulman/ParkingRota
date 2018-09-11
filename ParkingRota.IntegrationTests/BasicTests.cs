dde22da4f16ada0b1b9b19a905e5521e06240b01
Paul Bulman <paulbulman@gmail.com>
2018-09-11 18:27:20 +0100

    Temporary change to ensure failing test breaks build

--- ParkingRota.IntegrationTests/BasicTests.cs
+++ ParkingRota.IntegrationTests/BasicTests.cs
@@ -24,5 +24,8 @@ namespace ParkingRota.IntegrationTests
 
             Assert.Equal("text/html; charset=utf-8", response.Content.Headers.ContentType.ToString());
         }
+
+        [Fact]
+        public void Test_FailingTestBreaksBuild() => Assert.True(false);
     }
 }
