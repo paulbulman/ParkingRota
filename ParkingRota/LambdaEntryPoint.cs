4fa1f348fbba963ff03d97a0f69f48b95e0dc1f6
Paul Bulman <paulbulman@gmail.com>
2018-09-11 17:56:38 +0100

    Add support from AWS EBS/Lambda

---
+++ ParkingRota/LambdaEntryPoint.cs
@@ -0,0 +1,9 @@
+﻿namespace ParkingRota
+{
+    using Microsoft.AspNetCore.Hosting;
+
+    public class LambdaEntryPoint : Amazon.Lambda.AspNetCoreServer.APIGatewayProxyFunction
+    {
+        protected override void Init(IWebHostBuilder builder) => builder.UseStartup<Startup>();
+    }
+}
