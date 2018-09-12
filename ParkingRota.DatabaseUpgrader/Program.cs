4f2d0221aae7ec475bbdcafc99992a0d3cb0f335
Paul Bulman <paulbulman@gmail.com>
2018-09-12 07:46:05 +0100

    Create database upgrader project

---
+++ ParkingRota.DatabaseUpgrader/Program.cs
@@ -0,0 +1,49 @@
+﻿namespace ParkingRota.DatabaseUpgrader
+{
+    using System;
+    using System.Linq;
+    using System.Threading.Tasks;
+    using Data;
+    using Microsoft.EntityFrameworkCore;
+
+    internal static class Program
+    {
+        private static async Task Main()
+        {
+            var connectionString = Environment.GetEnvironmentVariable("AdminConnectionString");
+
+            if (string.IsNullOrEmpty(connectionString))
+            {
+                Console.WriteLine("Connection string environment variable not set.");
+                return;
+            }
+
+            var dbContextOptionsBuilder = new DbContextOptionsBuilder<ApplicationDbContext>()
+                .UseSqlServer(connectionString);
+
+            using (var context = new ApplicationDbContext(dbContextOptionsBuilder.Options))
+            {
+                var pendingMigrationNames = (await context.Database.GetPendingMigrationsAsync()).ToArray();
+
+                if (pendingMigrationNames.Any())
+                {
+                    Console.WriteLine("Applying the following pending database migrations:");
+
+                    foreach (var pendingMigrationName in pendingMigrationNames)
+                    {
+                        Console.WriteLine($"- {pendingMigrationName}");
+                    }
+
+                    await context.Database.MigrateAsync();
+
+                    Console.WriteLine();
+                    Console.WriteLine("Database migration complete.");
+                }
+                else
+                {
+                    Console.WriteLine("Database already up-to-date.");
+                }
+            }
+        }
+    }
+}
