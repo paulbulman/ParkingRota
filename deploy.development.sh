128eb64a00a0e3f037f6961c74bd50f55e003d3b
Paul Bulman <paulbulman@gmail.com>
2018-09-12 07:50:38 +0100

    Perform database upgrade during build

--- deploy.development.sh
+++ deploy.development.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
 if [ "$CODEBUILD_BUILD_SUCCEEDING" = "1" ]
 then
+  aws ec2 authorize-security-group-ingress --group-id $RdsSecurityGroupId --protocol tcp --port 1433 --cidr $CodeBuildCidr
+  dotnet run --project ParkingRota.DatabaseUpgrader
+  aws ec2 revoke-security-group-ingress --group-id $RdsSecurityGroupId --protocol tcp --port 1433 --cidr $CodeBuildCidr
   dotnet publish -c release
   cd ParkingRota
   dotnet lambda deploy-serverless
