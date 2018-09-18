802e30f8cc0cfed65c58d86c0f11e13a3ad6a31c
Paul Bulman <paulbulman@gmail.com>
2018-09-18 17:21:17 +0100

    Create password breach checker

---
+++ ParkingRota.Business/PasswordBreachChecker.cs
@@ -0,0 +1,68 @@
+﻿namespace ParkingRota.Business
+{
+    using System;
+    using System.Linq;
+    using System.Net.Http;
+    using System.Security.Cryptography;
+    using System.Text;
+    using System.Threading.Tasks;
+    using Microsoft.Extensions.Logging;
+
+    public class PasswordBreachChecker
+    {
+        private readonly ILogger<PasswordBreachChecker> logger;
+        private readonly HttpClient client;
+
+        public PasswordBreachChecker(ILogger<PasswordBreachChecker> logger, HttpClient client)
+        {
+            this.logger = logger;
+            this.client = client;
+        }
+
+        public async Task<bool> PasswordIsBreached(string password)
+        {
+            const string ErrorMessage =
+                "An error occurred checking whether the specified password was breached. Assuming password is okay.";
+
+            var hash = ComputeHash(password);
+
+            var hashPrefix = hash.Substring(0, 5);
+            var hashSuffix = hash.Substring(5);
+
+            try
+            {
+                var response = await this.CallApi(hashPrefix);
+                var responseContent = await response.Content.ReadAsStringAsync();
+
+                if (response.IsSuccessStatusCode)
+                {
+                    return ResponseContainsHashSuffix(responseContent, hashSuffix);
+                }
+
+                this.logger.LogError(ErrorMessage + $"\r\nThe response from the API was:\r\n{responseContent}");
+            }
+            catch (Exception exception)
+            {
+                this.logger.LogError(exception, ErrorMessage);
+            }
+
+            return false;
+        }
+
+        private static string ComputeHash(string password) =>
+            string.Join(
+                string.Empty,
+                SHA1.Create()
+                    .ComputeHash(Encoding.UTF8.GetBytes(password))
+                    .Select(b => b.ToString("X2")));
+
+        private async Task<HttpResponseMessage> CallApi(string hashPrefix) =>
+            await this.client.GetAsync($"https://api.pwnedpasswords.com/range/{hashPrefix}");
+
+        private static bool ResponseContainsHashSuffix(string responseContent, string hashSuffix) =>
+            responseContent
+                .Split("\r\n")
+                .Select(record => record.Split(":").First())
+                .Any(suffix => string.Equals(suffix, hashSuffix, StringComparison.InvariantCultureIgnoreCase));
+    }
+}
---
+++ ParkingRota.IntegrationTests/Business/PasswordBreachCheckerTests.cs
@@ -0,0 +1,26 @@
+﻿namespace ParkingRota.IntegrationTests.Business
+{
+    using System.Net.Http;
+    using System.Threading.Tasks;
+    using ParkingRota.Business;
+    using Xunit;
+
+    public class PasswordBreachCheckerTests
+    {
+        [Theory]
+        [InlineData("Password", true)]
+        [InlineData("LetMeIn", true)]
+        [InlineData("565B1390-35FC-4B7D-BF2D-B328A67CAEF3", false)]
+        public async Task Test_PasswordIsBreached(string password, bool expectIsBreached)
+        {
+            using (var client = new HttpClient())
+            {
+                var checker = new PasswordBreachChecker(null, client);
+
+                var isBreached = await checker.PasswordIsBreached(password);
+
+                Assert.Equal(expectIsBreached, isBreached);
+            }
+        }
+    }
+}
