02439cec571c3ee3bd935211536423bd5288db72
Paul Bulman <paulbulman@gmail.com>
2018-11-03 13:59:19 +0000

    Add request entity

--- ParkingRota.Data/ApplicationDbContext.cs
+++ ParkingRota.Data/ApplicationDbContext.cs
@@ -15,18 +15,24 @@
 
         public DbSet<RegistrationToken> RegistrationTokens { get; set; }
 
+        public DbSet<Request> Requests { get; set; }
+
         protected override void OnModelCreating(ModelBuilder builder)
         {
             base.OnModelCreating(builder);
 
             builder.Entity<ApplicationUser>().Property(a => a.CommuteDistance).HasColumnType("decimal(18,2)");
 
-            builder.Entity<BankHoliday>().HasIndex(a => a.DbDate).IsUnique();
-            builder.Entity<BankHoliday>().Property(a => a.DbDate).HasColumnName("Date");
-            builder.Entity<BankHoliday>().Ignore(a => a.Date);
+            builder.Entity<BankHoliday>().HasIndex(b => b.DbDate).IsUnique();
+            builder.Entity<BankHoliday>().Property(b => b.DbDate).HasColumnName("Date");
+            builder.Entity<BankHoliday>().Ignore(b => b.Date);
 
             builder.Entity<RegistrationToken>().Property(t => t.DbExpiryTime).HasColumnName("ExpiryTime");
             builder.Entity<RegistrationToken>().Ignore(t => t.ExpiryTime);
+
+            builder.Entity<Request>().HasIndex(r => new { r.ApplicationUserId, r.DbDate }).IsUnique();
+            builder.Entity<Request>().Property(r => r.DbDate).HasColumnName("Date");
+            builder.Entity<Request>().Ignore(r => r.Date);
         }
     }
 }
---
+++ ParkingRota.Data/Migrations/20181103135652_CreateRequestEntity.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ParkingRota.Data.Migrations
+{
+    public partial class CreateRequestEntity : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_BankHoliday",
+                table: "BankHoliday");
+
+            migrationBuilder.RenameTable(
+                name: "BankHoliday",
+                newName: "BankHolidays");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_BankHoliday_Date",
+                table: "BankHolidays",
+                newName: "IX_BankHolidays_Date");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_BankHolidays",
+                table: "BankHolidays",
+                column: "Id");
+
+            migrationBuilder.CreateTable(
+                name: "Requests",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    ApplicationUserId = table.Column<string>(nullable: false),
+                    Date = table.Column<DateTime>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Requests", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Requests_AspNetUsers_ApplicationUserId",
+                        column: x => x.ApplicationUserId,
+                        principalTable: "AspNetUsers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Requests_ApplicationUserId_Date",
+                table: "Requests",
+                columns: new[] { "ApplicationUserId", "Date" },
+                unique: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Requests");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_BankHolidays",
+                table: "BankHolidays");
+
+            migrationBuilder.RenameTable(
+                name: "BankHolidays",
+                newName: "BankHoliday");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_BankHolidays_Date",
+                table: "BankHoliday",
+                newName: "IX_BankHoliday_Date");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_BankHoliday",
+                table: "BankHoliday",
+                column: "Id");
+        }
+    }
+}
