c112ae1036655ae7a5b8addbd2da1ebc30f7904e
Paul Bulman <paulbulman@gmail.com>
2018-11-05 18:18:48 +0000

    Some changes to IndexModel

--- ParkingRota/Pages/Index.cshtml.cs
+++ ParkingRota/Pages/Index.cshtml.cs
@@ -1,17 +1,32 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.RazorPages;
-
-namespace ParkingRota.Pages
+﻿namespace ParkingRota.Pages
 {
+    using System.Collections.Generic;
+    using Calendar;
+    using Microsoft.AspNetCore.Mvc.RazorPages;
+    using NodaTime;
+
     public class IndexModel : PageModel
     {
         public void OnGet()
         {
 
+        }
+
+        public Calendar Calendar { get; private set; }
+
+        public IDictionary<LocalDate, IReadOnlyList<DisplayRequest>> DisplayRequests { get; private set; }
+
+        public class DisplayRequest
+        {
+            public DisplayRequest(string fullName, bool isCurrentUser)
+            {
+                this.FullName = fullName;
+                this.IsCurrentUser = isCurrentUser;
+            }
+
+            public string FullName { get; }
+
+            public bool IsCurrentUser { get; }
         }
     }
 }
