bc77e1ea1ac5365ba1da38cf37c8d53de48542b0
Paul Bulman <paulbulman@gmail.com>
2018-11-28 16:51:43 +0000

    Changes to default template

--- ParkingRota.Business/Model/ApplicationUser.cs
+++ ParkingRota.Business/Model/ApplicationUser.cs
@@ -5,21 +5,25 @@
 
     public class ApplicationUser : IdentityUser
     {
+        [ProtectedPersonalData]
         [Required]
         [StringLength(50)]
         [Display(Name = "First name")]
         public string FirstName { get; set; }
 
+        [ProtectedPersonalData]
         [Required]
         [StringLength(50)]
         [Display(Name = "Last name")]
         public string LastName { get; set; }
 
+        [ProtectedPersonalData]
         [Required]
         [StringLength(10)]
         [Display(Name = "Car registration number")]
         public string CarRegistrationNumber { get; set; }
 
+        [PersonalData]
         public decimal CommuteDistance { get; set; }
 
         public string FullName => $"{this.FirstName} {this.LastName}";
--- ParkingRota/Areas/Identity/Pages/Account/Manage/Index.cshtml.cs
+++ ParkingRota/Areas/Identity/Pages/Account/Manage/Index.cshtml.cs
@@ -2,11 +2,9 @@
 {
     using System;
     using System.ComponentModel.DataAnnotations;
-    using System.Text.Encodings.Web;
     using System.Threading.Tasks;
     using Business.Model;
     using Microsoft.AspNetCore.Identity;
-    using Microsoft.AspNetCore.Identity.UI.Services;
     using Microsoft.AspNetCore.Mvc;
     using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -14,21 +12,14 @@
     {
         private readonly UserManager<ApplicationUser> userManager;
         private readonly SignInManager<ApplicationUser> signInManager;
-        private readonly IEmailSender emailSender;
 
-        public IndexModel(
-            UserManager<ApplicationUser> userManager,
-            SignInManager<ApplicationUser> signInManager,
-            IEmailSender emailSender)
+        public IndexModel(UserManager<ApplicationUser> userManager, SignInManager<ApplicationUser> signInManager)
         {
             this.userManager = userManager;
             this.signInManager = signInManager;
-            this.emailSender = emailSender;
         }
 
         public string Username { get; set; }
-
-        public bool IsEmailConfirmed { get; set; }
 
         [TempData]
         public string StatusMessage { get; set; }
@@ -52,10 +43,6 @@
             [StringLength(10)]
             [Display(Name = "Car registration number")]
             public string CarRegistrationNumber { get; set; }
-
-            [Required]
-            [EmailAddress]
-            public string Email { get; set; }
         }
 
         public async Task<IActionResult> OnGetAsync()
@@ -67,7 +54,6 @@
             }
 
             var userName = await this.userManager.GetUserNameAsync(user);
-            var email = await this.userManager.GetEmailAsync(user);
 
             this.Username = userName;
 
@@ -76,10 +62,7 @@
                 FirstName = user.FirstName,
                 LastName = user.LastName,
                 CarRegistrationNumber = user.CarRegistrationNumber,
-                Email = email
             };
-
-            this.IsEmailConfirmed = await this.userManager.IsEmailConfirmedAsync(user);
 
             return this.Page();
         }
@@ -95,17 +78,6 @@
             if (user == null)
             {
                 return this.NotFound($"Unable to load user with ID '{this.userManager.GetUserId(this.User)}'.");
-            }
-
-            var email = await this.userManager.GetEmailAsync(user);
-            if (this.Input.Email != email)
-            {
-                var setEmailResult = await this.userManager.SetEmailAsync(user, this.Input.Email);
-                if (!setEmailResult.Succeeded)
-                {
-                    var userId = await this.userManager.GetUserIdAsync(user);
-                    throw new InvalidOperationException($"Unexpected error occurred setting email for user with ID '{userId}'.");
-                }
             }
 
             if (this.Input.FirstName != user.FirstName ||
@@ -125,37 +97,7 @@
             }
 
             await this.signInManager.RefreshSignInAsync(user);
-            this.StatusMessage = "Your profile has been updated";
-            return this.RedirectToPage();
-        }
-
-        public async Task<IActionResult> OnPostSendVerificationEmailAsync()
-        {
-            if (!this.ModelState.IsValid)
-            {
-                return this.Page();
-            }
-
-            var user = await this.userManager.GetUserAsync(this.User);
-            if (user == null)
-            {
-                return this.NotFound($"Unable to load user with ID '{this.userManager.GetUserId(this.User)}'.");
-            }
-
-            var userId = await this.userManager.GetUserIdAsync(user);
-            var email = await this.userManager.GetEmailAsync(user);
-            var code = await this.userManager.GenerateEmailConfirmationTokenAsync(user);
-            var callbackUrl = this.Url.Page(
-                "/Account/ConfirmEmail",
-                pageHandler: null,
-                values: new { userId = userId, code = code },
-                protocol: this.Request.Scheme);
-            await this.emailSender.SendEmailAsync(
-                email,
-                "Confirm your email",
-                $"Please confirm your account by <a href='{HtmlEncoder.Default.Encode(callbackUrl)}'>clicking here</a>.");
-
-            this.StatusMessage = "Verification email sent. Please check your email.";
+            this.StatusMessage = "Profile updated.";
             return this.RedirectToPage();
         }
     }
