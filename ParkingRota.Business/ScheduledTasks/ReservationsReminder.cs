9941cef136af1db5a36644c3a53d3eaa76788a2d
Paul Bulman <paulbulman@gmail.com>
2018-12-14 08:10:07 +0000

    Add reservations reminder scheduled task

---
+++ ParkingRota.Business/ScheduledTasks/ReservationsReminder.cs
@@ -0,0 +1,45 @@
+﻿namespace ParkingRota.Business.ScheduledTasks
+{
+    using System.Linq;
+    using System.Threading.Tasks;
+    using Microsoft.AspNetCore.Identity;
+    using Model;
+
+    public class ReservationsReminder
+    {
+        private readonly IDateCalculator dateCalculator;
+        private readonly IEmailRepository emailRepository;
+        private readonly IReservationRepository reservationRepository;
+        private readonly UserManager<ApplicationUser> userManager;
+
+        public ReservationsReminder(
+            IDateCalculator dateCalculator,
+            IEmailRepository emailRepository,
+            IReservationRepository reservationRepository,
+            UserManager<ApplicationUser> userManager)
+        {
+            this.dateCalculator = dateCalculator;
+            this.emailRepository = emailRepository;
+            this.reservationRepository = reservationRepository;
+            this.userManager = userManager;
+        }
+
+        public async Task Run()
+        {
+            var nextWorkingDate = this.dateCalculator.GetNextWorkingDate();
+
+            var reservations = this.reservationRepository.GetReservations(nextWorkingDate, nextWorkingDate);
+
+            if (!reservations.Any())
+            {
+                var teamLeaderUsers = await this.userManager.GetUsersInRoleAsync(UserRole.TeamLeader);
+
+                foreach (var teamLeaderUser in teamLeaderUsers)
+                {
+                    this.emailRepository.AddToQueue(
+                        new Emails.ReservationsReminder(teamLeaderUser.Email, nextWorkingDate));
+                }
+            }
+        }
+    }
+}
---
+++ ParkingRota.UnitTests/Business/ScheduledTasks/ReservationReminderTests.cs
@@ -0,0 +1,92 @@
+﻿namespace ParkingRota.UnitTests.Business.ScheduledTasks
+{
+    using System.Collections.Generic;
+    using System.Threading.Tasks;
+    using Moq;
+    using NodaTime.Testing.Extensions;
+    using ParkingRota.Business;
+    using ParkingRota.Business.Model;
+    using ParkingRota.Business.ScheduledTasks;
+    using Xunit;
+
+    public static class ReservationReminderTests
+    {
+        [Fact]
+        public static async Task Test_Run_NoReservationsEntered()
+        {
+            // Arrange
+            var date = 14.December(2018);
+
+            var mockDateCalculator = new Mock<IDateCalculator>(MockBehavior.Strict);
+            mockDateCalculator
+                .Setup(d => d.GetNextWorkingDate())
+                .Returns(date);
+
+            var mockReservationRepository = new Mock<IReservationRepository>(MockBehavior.Strict);
+            mockReservationRepository
+                .Setup(r => r.GetReservations(date, date))
+                .Returns(new List<Reservation>());
+
+            var mockEmailRepository = new Mock<IEmailRepository>(MockBehavior.Strict);
+            mockEmailRepository
+                .Setup(e => e.AddToQueue(It.IsAny<ParkingRota.Business.Emails.ReservationsReminder>()));
+
+            IList<ApplicationUser> teamLeaderUsers = new[]
+            {
+                new ApplicationUser { Email = "a@b.c" },
+                new ApplicationUser { Email = "x@y.z" }
+            };
+
+            var mockUserManager = TestHelpers.CreateMockUserManager();
+            mockUserManager
+                .Setup(u => u.GetUsersInRoleAsync(UserRole.TeamLeader))
+                .Returns(Task.FromResult(teamLeaderUsers));
+
+            // Act
+            var reservationReminder = new ReservationsReminder(
+                mockDateCalculator.Object,
+                mockEmailRepository.Object,
+                mockReservationRepository.Object,
+                mockUserManager.Object);
+
+            await reservationReminder.Run();
+
+            // Assert
+            foreach (var teamLeaderUser in teamLeaderUsers)
+            {
+                mockEmailRepository.Verify(
+                    r => r.AddToQueue(
+                        It.Is<ParkingRota.Business.Emails.ReservationsReminder>(e => e.To == teamLeaderUser.Email)),
+                    Times.Once);
+            }
+        }
+
+        [Fact]
+        public static async Task Test_Run_ReservationsAlreadyEntered()
+        {
+            // Arrange
+            var date = 14.December(2018);
+
+            var mockDateCalculator = new Mock<IDateCalculator>(MockBehavior.Strict);
+            mockDateCalculator
+                .Setup(d => d.GetNextWorkingDate())
+                .Returns(date);
+
+            var mockReservationRepository = new Mock<IReservationRepository>(MockBehavior.Strict);
+            mockReservationRepository
+                .Setup(r => r.GetReservations(date, date))
+                .Returns(new[] { new Reservation() });
+
+            var mockEmailRepository = new Mock<IEmailRepository>(MockBehavior.Strict).Object;
+
+            // Act and assert: mock strict on email repository ensures nothing has been done.
+            var reservationReminder = new ReservationsReminder(
+                mockDateCalculator.Object,
+                mockEmailRepository,
+                mockReservationRepository.Object,
+                TestHelpers.CreateMockUserManager().Object);
+
+            await reservationReminder.Run();
+        }
+    }
+}
