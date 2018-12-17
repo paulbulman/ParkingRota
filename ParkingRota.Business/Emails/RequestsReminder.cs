859c3856b44d5ccacba4781c5bd03142064886d9
Paul Bulman <paulbulman@gmail.com>
2018-12-17 19:16:07 +0000

    Add requests reminder email

---
+++ ParkingRota.Business/Emails/RequestsReminder.cs
@@ -0,0 +1,29 @@
+﻿namespace ParkingRota.Business.Emails
+{
+    using NodaTime;
+
+    public class RequestsReminder : IEmail
+    {
+        private readonly LocalDate firstDate;
+        private readonly LocalDate lastDate;
+
+        public RequestsReminder(string to, LocalDate firstDate, LocalDate lastDate)
+        {
+            this.To = to;
+
+            this.firstDate = firstDate;
+            this.lastDate = lastDate;
+        }
+
+        public string To { get; }
+
+        public string Subject => $"No requests entered for {this.firstDate.ForDisplay()} - {this.lastDate.ForDisplay()}";
+
+        public string HtmlBody => $"<p>{this.PlainTextBody}</p>";
+
+        public string PlainTextBody =>
+            $"No requests have yet been entered for {this.firstDate.ForDisplay()} - {this.lastDate.ForDisplay()}." +
+            " If you do not need parking during this period you can ignore this message." +
+            " Otherwise, you should enter requests by the end of today to have them taken into account.";
+    }
+}
---
+++ ParkingRota.UnitTests/Business/Emails/RequestsReminderTests.cs
@@ -0,0 +1,42 @@
+﻿namespace ParkingRota.UnitTests.Business.Emails
+{
+    using System;
+    using NodaTime;
+    using NodaTime.Testing.Extensions;
+    using ParkingRota.Business.Emails;
+    using Xunit;
+
+    public static class RequestsReminderTests
+    {
+        [Theory]
+        [InlineData("a@b.c")]
+        [InlineData("x@y.z")]
+        public static void TestTo(string to)
+        {
+            var email = new RequestsReminder(to, default(LocalDate), default(LocalDate));
+
+            Assert.Equal(to, email.To);
+        }
+
+        [Fact]
+        public static void TestSubject()
+        {
+            const string ExpectedSubject = "No requests entered for 17 Dec - 21 Dec";
+
+            var email = new RequestsReminder(default(string), 17.December(2018), 21.December(2018));
+
+            Assert.Equal(ExpectedSubject, email.Subject);
+        }
+
+        [Fact]
+        public static void TestBody()
+        {
+            const string ExpectedText = "No requests have yet been entered for 17 Dec - 21 Dec";
+
+            var email = new RequestsReminder(default(string), 17.December(2018), 21.December(2018));
+
+            Assert.True(email.HtmlBody.Contains(ExpectedText, StringComparison.InvariantCultureIgnoreCase));
+            Assert.True(email.PlainTextBody.Contains(ExpectedText, StringComparison.InvariantCultureIgnoreCase));
+        }
+    }
+}
