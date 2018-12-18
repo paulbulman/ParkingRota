4b90469da8f6a68f3ca50649cd3c55a7f21f6b9a
Paul Bulman <paulbulman@gmail.com>
2018-12-18 18:43:55 +0000

    Add daily summary email

---
+++ ParkingRota.Business/Emails/DailySummary.cs
@@ -0,0 +1,86 @@
+﻿namespace ParkingRota.Business.Emails
+{
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using Model;
+
+    public class DailySummary : IEmail
+    {
+        private readonly IReadOnlyList<Allocation> allocations;
+        private readonly IReadOnlyList<Request> requests;
+
+        public DailySummary(string to, IReadOnlyList<Allocation> allocations, IReadOnlyList<Request> requests)
+        {
+            this.allocations = allocations;
+            this.requests = requests;
+            this.To = to;
+        }
+
+        public string To { get; }
+
+        public string Subject => $"Daily allocations summary for {this.FormattedDate}";
+
+        public string HtmlBody
+        {
+            get
+            {
+                var body = new StringBuilder();
+
+                body.Append($"<p>{this.Header}</p>");
+
+                body.Append($"<p>{string.Join("<br/>", this.AllocatedNames)}</p>");
+
+                if (this.InterruptedNames.Any())
+                {
+                    body.Append($"<p>(Interrupted: {string.Join(", ", this.InterruptedNames)})</p>");
+                }
+
+                body.Append($"<p>{Footer}</p>");
+
+                return body.ToString();
+            }
+        }
+
+        public string PlainTextBody
+        {
+            get
+            {
+                var lines = new List<string>();
+
+                lines.Add(this.Header);
+                lines.Add(string.Empty);
+
+                lines.AddRange(this.AllocatedNames);
+                lines.Add(string.Empty);
+
+                if (this.InterruptedNames.Any())
+                {
+                    lines.Add($"(Interrupted: {string.Join(", ", this.InterruptedNames)})");
+                    lines.Add(string.Empty);
+                }
+
+                lines.Add(Footer);
+
+                return string.Join("\n", lines);
+            }
+        }
+
+        private IEnumerable<string> AllocatedNames => this.allocations
+            .OrderBy(a => a.ApplicationUser.LastName)
+            .Select(a => a.ApplicationUser.FullName);
+
+        private IEnumerable<string> InterruptedNames => this.requests
+            .Where(r => this.allocations.All(a => a.ApplicationUser.Id != r.ApplicationUser.Id))
+            .OrderBy(i => i.ApplicationUser.LastName)
+            .Select(i => i.ApplicationUser.FullName);
+
+        private string FormattedDate => this.allocations.First().Date.ForDisplay();
+
+        private string Header => $"Allocations for {this.FormattedDate}, after day-ahead spaces released:";
+
+        private const string Footer =
+            "If you no longer need a space allocated to you, " +
+            "please remove your request so that it can be given to someone else.";
+    }
+}
---
+++ ParkingRota.UnitTests/Business/Emails/DailySummaryTests.cs
@@ -0,0 +1,70 @@
+﻿namespace ParkingRota.UnitTests.Business.Emails
+{
+    using System;
+    using System.Collections.Generic;
+    using NodaTime.Testing.Extensions;
+    using ParkingRota.Business.Emails;
+    using ParkingRota.Business.Model;
+    using Xunit;
+
+    public static class DailySummaryTests
+    {
+        [Theory]
+        [InlineData("a@b.c")]
+        [InlineData("x@y.z")]
+        public static void TestTo(string to)
+        {
+            var email = new DailySummary(to, default(IReadOnlyList<Allocation>), default(IReadOnlyList<Request>));
+
+            Assert.Equal(to, email.To);
+        }
+
+        [Fact]
+        public static void TestSubject()
+        {
+            var allocations = new[] { new Allocation { Date = 18.December(2018) } };
+
+            const string ExpectedSubject = "Daily allocations summary for 18 Dec";
+
+            var email = new DailySummary(default(string), allocations, default(IReadOnlyList<Request>));
+
+            Assert.Equal(ExpectedSubject, email.Subject);
+        }
+
+        [Fact]
+        public static void TestBody()
+        {
+            var allocatedUser = new ApplicationUser { FirstName = "Petr", LastName = "Čech" };
+            var otherAllocatedUser = new ApplicationUser { FirstName = "Héctor", LastName = "Bellerín" };
+
+            var interruptedUser = new ApplicationUser { FirstName = "Sokratis", LastName = "Papastathopoulos" };
+            var otherInterruptedUser = new ApplicationUser { FirstName = "Mohamed", LastName = "Elneny" };
+
+            var allocations = new[]
+            {
+                new Allocation { ApplicationUser = allocatedUser, Date = 18.December(2018) },
+                new Allocation { ApplicationUser = otherAllocatedUser, Date = 18.December(2018) },
+            };
+
+            var requests = new[]
+            {
+                new Request { ApplicationUser = allocatedUser, Date = 18.December(2018) },
+                new Request { ApplicationUser = otherAllocatedUser, Date = 18.December(2018) },
+                new Request { ApplicationUser = interruptedUser, Date = 18.December(2018) },
+                new Request { ApplicationUser = otherInterruptedUser, Date = 18.December(2018) },
+            };
+
+            var email = new DailySummary(default(string), allocations, requests);
+
+            Assert.True(email.HtmlBody.Contains(allocatedUser.FullName, StringComparison.InvariantCultureIgnoreCase));
+            Assert.True(email.HtmlBody.Contains(otherAllocatedUser.FullName, StringComparison.InvariantCultureIgnoreCase));
+            Assert.True(email.PlainTextBody.Contains(allocatedUser.FullName, StringComparison.InvariantCultureIgnoreCase));
+            Assert.True(email.PlainTextBody.Contains(otherAllocatedUser.FullName, StringComparison.InvariantCultureIgnoreCase));
+
+            var interruptedText = $"(Interrupted: {otherInterruptedUser.FullName}, {interruptedUser.FullName})";
+
+            Assert.True(email.HtmlBody.Contains(interruptedText, StringComparison.InvariantCultureIgnoreCase));
+            Assert.True(email.PlainTextBody.Contains(interruptedText, StringComparison.InvariantCultureIgnoreCase));
+        }
+    }
+}
