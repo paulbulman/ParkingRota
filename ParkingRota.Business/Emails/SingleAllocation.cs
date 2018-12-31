4479808ebedede4b350405506cfb3be67454949d
Paul Bulman <paulbulman@gmail.com>
2018-12-31 18:20:31 +0000

    Add single allocation notification email

---
+++ ParkingRota.Business/Emails/SingleAllocation.cs
@@ -0,0 +1,21 @@
+﻿namespace ParkingRota.Business.Emails
+{
+    using Model;
+
+    public class SingleAllocation : IEmail
+    {
+        private readonly Allocation allocation;
+
+        public SingleAllocation(Allocation allocation) => this.allocation = allocation;
+
+        public string To => this.allocation.ApplicationUser.Email;
+
+        public string Subject => $"Space available on {this.allocation.Date.ForDisplay()}";
+
+        public string HtmlBody => $"<p>{this.PlainTextBody}</p>";
+
+        public string PlainTextBody =>
+            $"A space has been allocated to you for {this.allocation.Date.ForDisplay()}. " +
+            "If you no longer need this space, please remove your request so that it can be given to someone else.";
+    }
+}
---
+++ ParkingRota.UnitTests/Business/Emails/SingleAllocationTests.cs
@@ -0,0 +1,42 @@
+﻿namespace ParkingRota.UnitTests.Business.Emails
+{
+    using System;
+    using NodaTime.Testing.Extensions;
+    using ParkingRota.Business.Emails;
+    using ParkingRota.Business.Model;
+    using Xunit;
+
+    public static class SingleAllocationTests
+    {
+        [Theory]
+        [InlineData("a@b.c")]
+        [InlineData("x@y.z")]
+        public static void TestTo(string to)
+        {
+            var email = new SingleAllocation(new Allocation { ApplicationUser = new ApplicationUser { Email = to } });
+
+            Assert.Equal(to, email.To);
+        }
+
+        [Fact]
+        public static void TestSubject()
+        {
+            const string ExpectedSubject = "Space available on 18 Dec";
+
+            var email = new SingleAllocation(new Allocation { Date = 18.December(2018) });
+
+            Assert.Equal(ExpectedSubject, email.Subject);
+        }
+
+        [Fact]
+        public static void TestBody()
+        {
+            const string ExpectedBody = "A space has been allocated to you for 18 Dec.";
+
+            var email = new SingleAllocation(new Allocation { Date = 18.December(2018) });
+
+            Assert.True(email.HtmlBody.Contains(ExpectedBody, StringComparison.OrdinalIgnoreCase));
+            Assert.True(email.PlainTextBody.Contains(ExpectedBody, StringComparison.OrdinalIgnoreCase));
+        }
+    }
+}
