41e547215f41a0e0130c38e51a27e106d81ee87e
Paul Bulman <paulbulman@gmail.com>
2018-12-31 18:18:12 +0000

    Add daily summary scheduled task

---
+++ ParkingRota.Business/ScheduledTasks/DailySummary.cs
@@ -0,0 +1,50 @@
+﻿namespace ParkingRota.Business.ScheduledTasks
+{
+    using System.Linq;
+    using System.Threading.Tasks;
+    using Model;
+    using NodaTime;
+
+    public class DailySummary : IScheduledTask
+    {
+        private readonly IAllocationRepository allocationRepository;
+        private readonly IDateCalculator dateCalculator;
+        private readonly IEmailRepository emailRepository;
+        private readonly IRequestRepository requestRepository;
+
+        public DailySummary(
+            IAllocationRepository allocationRepository,
+            IDateCalculator dateCalculator,
+            IEmailRepository emailRepository,
+            IRequestRepository requestRepository)
+        {
+            this.allocationRepository = allocationRepository;
+            this.dateCalculator = dateCalculator;
+            this.emailRepository = emailRepository;
+            this.requestRepository = requestRepository;
+        }
+
+        public ScheduledTaskType ScheduledTaskType => ScheduledTaskType.DailySummary;
+
+        public Task Run()
+        {
+            var nextWorkingDate = this.dateCalculator.GetNextWorkingDate();
+
+            var allocations = this.allocationRepository.GetAllocations(nextWorkingDate, nextWorkingDate);
+            var requests = this.requestRepository.GetRequests(nextWorkingDate, nextWorkingDate);
+
+            foreach (var applicationUser in requests.Select(r => r.ApplicationUser))
+            {
+                this.emailRepository.AddToQueue(new Emails.DailySummary(applicationUser.Email, allocations, requests));
+            }
+
+            return Task.CompletedTask;
+        }
+
+        public Instant GetNextRunTime(Instant currentInstant) =>
+            this.dateCalculator.GetNextWorkingDate()
+                .At(new LocalTime(11, 0, 0))
+                .InZoneStrictly(this.dateCalculator.TimeZone)
+                .ToInstant();
+    }
+}
---
+++ ParkingRota.UnitTests/Business/ScheduledTasks/DailySummaryTests.cs
@@ -0,0 +1,108 @@
+﻿namespace ParkingRota.UnitTests.Business.ScheduledTasks
+{
+    using Moq;
+    using NodaTime;
+    using NodaTime.Testing.Extensions;
+    using ParkingRota.Business;
+    using ParkingRota.Business.Emails;
+    using ParkingRota.Business.Model;
+    using Xunit;
+    using DailySummary = ParkingRota.Business.ScheduledTasks.DailySummary;
+
+    public static class DailySummaryTests
+    {
+        [Fact]
+        public static void Test_ScheduledTaskType()
+        {
+            var result = new DailySummary(
+                Mock.Of<IAllocationRepository>(),
+                Mock.Of<IDateCalculator>(),
+                Mock.Of<IEmailRepository>(),
+                Mock.Of<IRequestRepository>()).ScheduledTaskType;
+
+            Assert.Equal(ScheduledTaskType.DailySummary, result);
+        }
+
+        [Fact]
+        public static async void Test_Run()
+        {
+            // Arrange
+            var nextWorkingDate = 28.December(2018);
+
+            var allocatedUser = new ApplicationUser { Email = "a@b.c" };
+            var interruptedUser = new ApplicationUser { Email = "x@y.z" };
+
+            var allocations = new[] { new Allocation { ApplicationUser = allocatedUser, Date = nextWorkingDate } };
+            var requests = new[]
+            {
+                new Request { ApplicationUser = allocatedUser, Date = nextWorkingDate },
+                new Request { ApplicationUser = interruptedUser, Date = nextWorkingDate }
+            };
+
+            var mockAllocationRepository = new Mock<IAllocationRepository>(MockBehavior.Strict);
+            mockAllocationRepository
+                .Setup(a => a.GetAllocations(nextWorkingDate, nextWorkingDate))
+                .Returns(allocations);
+
+            var mockDateCalculator = new Mock<IDateCalculator>(MockBehavior.Strict);
+            mockDateCalculator
+                .Setup(d => d.GetNextWorkingDate())
+                .Returns(nextWorkingDate);
+
+            var mockEmailRepository = new Mock<IEmailRepository>(MockBehavior.Strict);
+            mockEmailRepository.Setup(e => e.AddToQueue(It.IsAny<IEmail>()));
+
+            var mockRequestRepository = new Mock<IRequestRepository>(MockBehavior.Strict);
+            mockRequestRepository
+                .Setup(r => r.GetRequests(nextWorkingDate, nextWorkingDate))
+                .Returns(requests);
+
+            // Act
+            var dailySummary = new DailySummary(
+                mockAllocationRepository.Object,
+                mockDateCalculator.Object,
+                mockEmailRepository.Object,
+                mockRequestRepository.Object);
+
+            await dailySummary.Run();
+
+            // Assert
+            foreach (var applicationUser in new[] { allocatedUser, interruptedUser })
+            {
+                mockEmailRepository.Verify(
+                    e => e.AddToQueue(
+                        It.Is<ParkingRota.Business.Emails.DailySummary>(s => s.To == applicationUser.Email)),
+                    Times.Once);
+            }
+        }
+
+        [Theory]
+        [InlineData(22, 11, 23, 11)]
+        [InlineData(23, 11, 26, 10)]
+        public static void Test_GetNextRunTime(int currentDay, int currentHour, int expectedDay, int expectedHour)
+        {
+            // Arrange
+            var mockDateCalculator = new Mock<IDateCalculator>(MockBehavior.Strict);
+            mockDateCalculator
+                .SetupGet(d => d.TimeZone)
+                .Returns(DateTimeZoneProviders.Tzdb["Europe/London"]);
+            mockDateCalculator
+                .Setup(d => d.GetNextWorkingDate())
+                .Returns(expectedDay.March(2018));
+
+            var reservationReminder = new DailySummary(
+                Mock.Of<IAllocationRepository>(),
+                mockDateCalculator.Object,
+                Mock.Of<IEmailRepository>(),
+                Mock.Of<IRequestRepository>());
+
+            // Act
+            var result = reservationReminder.GetNextRunTime(currentDay.March(2018).At(currentHour, 00, 00).Utc());
+
+            // Assert
+            var expected = expectedDay.March(2018).At(expectedHour, 00, 00).Utc();
+
+            Assert.Equal(expected, result);
+        }
+    }
+}
