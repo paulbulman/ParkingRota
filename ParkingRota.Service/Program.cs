01861dfa167b4f13e6eb907802f82458ad1938ef
Paul Bulman <paulbulman@gmail.com>
2019-01-01 18:53:10 +0000

    Wire up service

--- ParkingRota.Service/Program.cs
+++ ParkingRota.Service/Program.cs
@@ -2,16 +2,72 @@ namespace ParkingRota.Service
 {
     using System;
     using Amazon.Lambda.Core;
+    using AutoMapper;
+    using Business;
+    using Business.Model;
+    using Business.ScheduledTasks;
+    using Data;
+    using Microsoft.AspNetCore.Identity;
+    using Microsoft.EntityFrameworkCore;
+    using Microsoft.Extensions.Configuration;
     using Microsoft.Extensions.DependencyInjection;
     using NodaTime;
 
     public class Program
     {
-        public void RunTasks(ILambdaContext context)
+        public async void RunTasks(ILambdaContext context)
         {
             var services = new ServiceCollection();
 
+            var connectionString =
+                Environment.GetEnvironmentVariable("ParkingRotaConnectionString") ??
+                GetConfiguration().GetConnectionString("DefaultConnection");
+
+            services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(connectionString));
+
+            services.AddIdentity<ApplicationUser, IdentityRole>()
+                .AddEntityFrameworkStores<ApplicationDbContext>();
+
             services.AddSingleton<IClock>(SystemClock.Instance);
+
+            services.AddScoped<AllocationCreator>();
+            services.AddScoped<AllocationNotifier>();
+            services.AddScoped<IAllocationRepository, AllocationRepository>();
+            services.AddScoped<IApplicationDbContext, ApplicationDbContext>();
+            services.AddScoped<IBankHolidayRepository, BankHolidayRepository>();
+            services.AddScoped<IDateCalculator, DateCalculator>();
+            services.AddScoped<EmailProcessor>();
+            services.AddScoped<IEmailRepository, EmailRepository>();
+            services.AddScoped<IEmailSender, EmailSender>();
+            services.AddHttpClient<IPasswordBreachChecker, PasswordBreachChecker>();
+            services.AddScoped<IRegistrationTokenRepository, RegistrationTokenRepository>();
+            services.AddScoped<IRegistrationTokenValidator, RegistrationTokenValidator>();
+            services.AddScoped<IRequestRepository, RequestRepository>();
+            services.AddScoped<IRequestSorter, RequestSorter>();
+            services.AddScoped<IReservationRepository, ReservationRepository>();
+            services.AddScoped<ISingleDayAllocationCreator, SingleDayAllocationCreator>();
+            services.AddScoped<IScheduledTaskRepository, ScheduledTaskRepository>();
+            services.AddScoped<ScheduledTaskRunner>();
+            services.AddScoped<ISystemParameterListRepository, SystemParameterListRepository>();
+            
+            services.AddScoped<IScheduledTask, DailySummary>();
+            services.AddScoped<IScheduledTask, RequestReminder>();
+            services.AddScoped<IScheduledTask, ReservationReminder>();
+            services.AddScoped<IScheduledTask, WeeklySummary>();
+
+            var mapperConfiguration = new MapperConfiguration(c =>
+            {
+                c.CreateMap<Data.Allocation, Business.Model.Allocation>();
+                c.CreateMap<Data.BankHoliday, Business.Model.BankHoliday>();
+                c.CreateMap<Data.EmailQueueItem, Business.Model.EmailQueueItem>();
+                c.CreateMap<Data.RegistrationToken, Business.Model.RegistrationToken>();
+                c.CreateMap<Data.Request, Business.Model.Request>();
+                c.CreateMap<Data.Reservation, Business.Model.Reservation>();
+                c.CreateMap<Data.ScheduledTask, Business.Model.ScheduledTask>();
+                c.CreateMap<Data.SystemParameterList, Business.Model.SystemParameterList>();
+            });
+
+            services.AddSingleton<IMapper>(new Mapper(mapperConfiguration));
 
             var serviceScopeFactory = services
                 .BuildServiceProvider()
@@ -19,10 +75,23 @@ namespace ParkingRota.Service
 
             using (var scope = serviceScopeFactory.CreateScope())
             {
-                var clock = scope.ServiceProvider.GetRequiredService<IClock>();
+                var allocationCreator = scope.ServiceProvider.GetRequiredService<AllocationCreator>();
+                var newAllocations = allocationCreator.Create();
 
-                Console.WriteLine($"Service running at {clock.GetCurrentInstant()}");
+                var allocationNotifier = scope.ServiceProvider.GetRequiredService<AllocationNotifier>();
+                allocationNotifier.Notify(newAllocations);
+
+                var scheduledTaskRunner = scope.ServiceProvider.GetRequiredService<ScheduledTaskRunner>();
+                await scheduledTaskRunner.Run();
+
+                var emailProcessor = scope.ServiceProvider.GetRequiredService<EmailProcessor>();
+                await emailProcessor.SendPending();
             }
         }
+
+        private static IConfiguration GetConfiguration() =>
+            new ConfigurationBuilder()
+                .SetBasePath(AppContext.BaseDirectory)
+                .AddJsonFile("AppSettings.json").Build();
     }
 }
