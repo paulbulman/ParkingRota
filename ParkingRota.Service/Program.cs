c6ff543db389f783e5d1272e06fc05f40a6d49d4
Paul Bulman <paulbulman@gmail.com>
2019-01-22 12:41:42 +0000

    Cope with Lambda reusing same service process

--- ParkingRota.Service/Program.cs
+++ ParkingRota.Service/Program.cs
@@ -17,8 +17,30 @@ namespace ParkingRota.Service
 
     public class Program
     {
+        private readonly Lazy<ServiceProvider> serviceProvider = new Lazy<ServiceProvider>(BuildServiceProvider);
+
         public async Task RunTasks(ILambdaContext context)
         {
+            using (var scope = this.serviceProvider.Value.GetRequiredService<IServiceScopeFactory>().CreateScope())
+            {
+                var allocationCreator = scope.ServiceProvider.GetRequiredService<AllocationCreator>();
+                var newAllocations = allocationCreator.Create();
+
+                var allocationNotifier = scope.ServiceProvider.GetRequiredService<AllocationNotifier>();
+                allocationNotifier.Notify(newAllocations);
+
+                var scheduledTaskRunner = scope.ServiceProvider.GetRequiredService<ScheduledTaskRunner>();
+                await scheduledTaskRunner.Run();
+
+                var emailProcessor = scope.ServiceProvider.GetRequiredService<EmailProcessor>();
+                await emailProcessor.SendPending();
+            }
+        }
+
+        private static ServiceProvider BuildServiceProvider()
+        {
+            Console.WriteLine("Building service provider");
+
             var services = new ServiceCollection();
 
             services.AddLogging(configure => configure.AddConsole());
@@ -59,36 +81,22 @@ namespace ParkingRota.Service
             services.AddScoped<IScheduledTask, ReservationReminder>();
             services.AddScoped<IScheduledTask, WeeklySummary>();
 
-            var mapperConfiguration = new MapperConfiguration(c =>
-            {
-                c.CreateMap<Data.Allocation, Business.Model.Allocation>();
-                c.CreateMap<Data.BankHoliday, Business.Model.BankHoliday>();
-                c.CreateMap<Data.EmailQueueItem, Business.Model.EmailQueueItem>();
-                c.CreateMap<Data.RegistrationToken, Business.Model.RegistrationToken>();
-                c.CreateMap<Data.Request, Business.Model.Request>();
-                c.CreateMap<Data.Reservation, Business.Model.Reservation>();
-                c.CreateMap<Data.ScheduledTask, Business.Model.ScheduledTask>();
-                c.CreateMap<Data.SystemParameterList, Business.Model.SystemParameterList>();
-            });
+            var mapperConfiguration = new MapperConfiguration(
+                c =>
+                {
+                    c.CreateMap<Data.Allocation, Business.Model.Allocation>();
+                    c.CreateMap<Data.BankHoliday, Business.Model.BankHoliday>();
+                    c.CreateMap<Data.EmailQueueItem, Business.Model.EmailQueueItem>();
+                    c.CreateMap<Data.RegistrationToken, Business.Model.RegistrationToken>();
+                    c.CreateMap<Data.Request, Business.Model.Request>();
+                    c.CreateMap<Data.Reservation, Business.Model.Reservation>();
+                    c.CreateMap<Data.ScheduledTask, Business.Model.ScheduledTask>();
+                    c.CreateMap<Data.SystemParameterList, Business.Model.SystemParameterList>();
+                });
 
             services.AddSingleton<IMapper>(new Mapper(mapperConfiguration));
 
-            var serviceProvider = services.BuildServiceProvider();
-
-            using (var scope = serviceProvider.GetRequiredService<IServiceScopeFactory>().CreateScope())
-            {
-                var allocationCreator = scope.ServiceProvider.GetRequiredService<AllocationCreator>();
-                var newAllocations = allocationCreator.Create();
-
-                var allocationNotifier = scope.ServiceProvider.GetRequiredService<AllocationNotifier>();
-                allocationNotifier.Notify(newAllocations);
-
-                var scheduledTaskRunner = scope.ServiceProvider.GetRequiredService<ScheduledTaskRunner>();
-                await scheduledTaskRunner.Run();
-
-                var emailProcessor = scope.ServiceProvider.GetRequiredService<EmailProcessor>();
-                await emailProcessor.SendPending();
-            }
+            return services.BuildServiceProvider();
         }
 
         private static IConfiguration GetConfiguration() =>
