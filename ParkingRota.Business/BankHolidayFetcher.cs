91f6976fad75fdd34fdb701cdae93dffb0ffd6d0
Paul Bulman <paulbulman@gmail.com>
2019-02-02 11:02:12 +0000

    Create bank holiday fetcher

---
+++ ParkingRota.Business/BankHolidayFetcher.cs
@@ -0,0 +1,77 @@
+﻿namespace ParkingRota.Business
+{
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Net.Http;
+    using System.Threading.Tasks;
+    using Newtonsoft.Json;
+    using NodaTime;
+    using NodaTime.Text;
+
+    public class BankHolidayFetcher
+    {
+        private readonly HttpClient client;
+
+        public BankHolidayFetcher(HttpClient client) => this.client = client;
+
+        public async Task<IReadOnlyList<LocalDate>> Fetch()
+        {
+            const string Url = "https://www.gov.uk/bank-holidays.json";
+
+            var response = await this.client.GetAsync(Url);
+            var responseContent = await response.Content.ReadAsStringAsync();
+
+            return JsonConvert.DeserializeObject<BankHolidayData>(responseContent)
+                .EnglandAndWales
+                .Events
+                .Select(GetEventDate)
+                .ToArray();
+        }
+
+        private static LocalDate GetEventDate(Event eventData) =>
+            LocalDatePattern.Iso.Parse(eventData.Date).GetValueOrThrow();
+
+        private class BankHolidayData
+        {
+            [JsonProperty(PropertyName = "england-and-wales")]
+            public EnglandAndWales EnglandAndWales { get; set; }
+
+            public Scotland Scotland { get; set; }
+
+            [JsonProperty(PropertyName = "northern-ireland")]
+            public NorthernIreland NorthernIreland { get; set; }
+        }
+
+        private class EnglandAndWales
+        {
+            public string Division { get; set; }
+
+            public List<Event> Events { get; set; }
+        }
+
+        private class Scotland
+        {
+            public string Division { get; set; }
+
+            public List<Event> Events { get; set; }
+        }
+
+        private class NorthernIreland
+        {
+            public string Division { get; set; }
+
+            public List<Event> Events { get; set; }
+        }
+
+        private class Event
+        {
+            public string Title { get; set; }
+
+            public string Date { get; set; }
+
+            public string Notes { get; set; }
+
+            public bool Bunting { get; set; }
+        }
+    }
+}
---
+++ ParkingRota.IntegrationTests/Business/BankHolidayFetcherTests.cs
@@ -0,0 +1,37 @@
+﻿namespace ParkingRota.IntegrationTests.Business
+{
+    using System.Net.Http;
+    using System.Threading.Tasks;
+    using NodaTime.Testing.Extensions;
+    using ParkingRota.Business;
+    using Xunit;
+
+    public class BankHolidayFetcherTests
+    {
+        [Fact]
+        public async Task Test_EnglishBankHolidayIsReturned()
+        {
+            using (var client = new HttpClient())
+            {
+                var fetcher = new BankHolidayFetcher(client);
+
+                var result = await fetcher.Fetch();
+
+                Assert.Contains(25.December(2018), result);
+            }
+        }
+
+        [Fact]
+        public async Task Test_NorthernIrishBankHolidayIsNotReturned()
+        {
+            using (var client = new HttpClient())
+            {
+                var fetcher = new BankHolidayFetcher(client);
+
+                var result = await fetcher.Fetch();
+
+                Assert.DoesNotContain(12.July(2018), result);
+            }
+        }
+    }
+}
