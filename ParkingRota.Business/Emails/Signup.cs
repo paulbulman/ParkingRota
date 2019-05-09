69949193efce5d5c59c5820e199b06fdf7e46ec7
Paul Bulman <paulbulman@gmail.com>
2019-05-09 12:48:01 +0100

    Add signup email template

---
+++ ParkingRota.Business/Emails/Signup.cs
@@ -0,0 +1,61 @@
+﻿namespace ParkingRota.Business.Emails
+{
+    using System.Collections.Generic;
+    using System.Text.Encodings.Web;
+
+    public class Signup : IEmail
+    {
+        private readonly string unencodedCallbackUrl;
+        private readonly string originatingIpAddress;
+
+        public Signup(string to, string unencodedCallbackUrl, string originatingIpAddress)
+        {
+            this.To = to;
+
+            this.unencodedCallbackUrl = unencodedCallbackUrl;
+            this.originatingIpAddress = originatingIpAddress;
+        }
+
+        public string To { get; }
+
+        public string Subject => "Parking Rota registration";
+
+        public string HtmlBody
+        {
+            get
+            {
+                var encodedCallbackUrl = HtmlEncoder.Default.Encode(this.unencodedCallbackUrl);
+
+                return
+                    "<p>A registration token has been generated for you to create an account on the Parking Rota website.</p>" +
+                    $"<p>If you were expecting this, create an account by <a href='{encodedCallbackUrl}'>clicking here</a>." +
+                    "If not, you can disregard this email. The link will be valid for 24 hours.</p>" +
+                    $"<p>The request originated from IP address {this.originatingIpAddress}</p>";
+            }
+        }
+
+        public string PlainTextBody
+        {
+            get
+            {
+                var lines = new List<string>();
+
+                lines.Add("A registration token has been generated for you to create an account on the Parking Rota website.");
+                lines.Add(string.Empty);
+
+                lines.Add(
+                    "If you were expecting this, create an account by copying the following link (removing line breaks) " +
+                    "into your browser. If not, you can disregard this email. The link will be valid for 24 hours.");
+                lines.Add(string.Empty);
+
+                lines.Add("Registration link:");
+                lines.Add(this.unencodedCallbackUrl);
+                lines.Add(string.Empty);
+
+                lines.Add($"The request originated from IP address {this.originatingIpAddress}");
+
+                return string.Join("\n", lines);
+            }
+        }
+    }
+}
---
+++ ParkingRota.UnitTests/Business/Emails/SignupTests.cs
@@ -0,0 +1,41 @@
+﻿namespace ParkingRota.UnitTests.Business.Emails
+{
+    using System;
+    using ParkingRota.Business.Emails;
+    using Xunit;
+
+    public static class SignupTests
+    {
+        [Theory]
+        [InlineData("a@b.c")]
+        [InlineData("x@y.z")]
+        public static void TestTo(string to)
+        {
+            var email = new Signup(to, default(string), default(string));
+
+            Assert.Equal(to, email.To);
+        }
+
+        [Fact]
+        public static void TestSubject()
+        {
+            var email = new Signup(default(string), default(string), default(string));
+
+            Assert.Equal("Parking Rota registration", email.Subject);
+        }
+
+        [Theory]
+        [InlineData("https://localhost/Identity/Account/Register?registrationToken=abc", "1.2.3.4")]
+        [InlineData("https://localhost/Identity/Account/Register?registrationToken=xyz", "192.168.0.1")]
+        public static void TestBody(string callbackUrl, string originatingIpAddress)
+        {
+            var email = new Signup(default(string), callbackUrl, originatingIpAddress);
+
+            Assert.True(email.HtmlBody.Contains(callbackUrl.Replace("&", "&amp;"), StringComparison.Ordinal));
+            Assert.True(email.HtmlBody.Contains(originatingIpAddress, StringComparison.OrdinalIgnoreCase));
+
+            Assert.True(email.PlainTextBody.Contains(callbackUrl, StringComparison.Ordinal));
+            Assert.True(email.PlainTextBody.Contains(originatingIpAddress, StringComparison.OrdinalIgnoreCase));
+        }
+    }
+}
