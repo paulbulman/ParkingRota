027cb8b03ce3a4224d10caa7b721316356442e6c
Paul Bulman <paulbulman@gmail.com>
2019-07-12 08:09:44 +0100

    Re-add ability to run service as AWS Lambda function

---
+++ ParkingRota.Service/LambdaEntryPoint.cs
@@ -0,0 +1,14 @@
+﻿namespace ParkingRota.Service
+{
+    using System.Threading.Tasks;
+    using Amazon.Lambda.Core;
+
+    public class LambdaEntryPoint
+    {
+        private readonly Service service;
+
+        public LambdaEntryPoint() => this.service = new Service();
+
+        public async Task RunTasks(ILambdaContext context) => await this.service.RunTasksAsync();
+    }
+}
