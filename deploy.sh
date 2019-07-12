319e83bcd381782f860993e9be6ae025307c9d3f
Paul Bulman <paulbulman@gmail.com>
2019-07-12 11:25:12 +0100

    Update AWS CodeBuild files

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,11 @@
 #!/bin/sh
 if [ "$CODEBUILD_BUILD_SUCCEEDING" = "1" ]
 then
-  cd ParkingRota
+  dotnet run --project ParkingRota.DatabaseUpgrader -e=ParkingRotaAdminConnectionString
+  
+  cd ParkingRota.Service
+  dotnet lambda deploy-function
+  
+  cd ../ParkingRota
   dotnet eb deploy-environment
 fi
