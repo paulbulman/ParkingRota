6bda576363412ec10e099e5aefbad42736f5cf06
Paul Bulman <paulbulman@gmail.com>
2019-07-16 07:36:35 +0100

    Get service connection string from parameter store

--- ParkingRota.Service/LambdaEntryPoint.cs
+++ ParkingRota.Service/LambdaEntryPoint.cs
@@ -1,14 +1,36 @@
 ﻿namespace ParkingRota.Service
 {
     using System.Threading.Tasks;
+    using Amazon;
     using Amazon.Lambda.Core;
+    using Amazon.SimpleSystemsManagement;
+    using Amazon.SimpleSystemsManagement.Model;
 
     public class LambdaEntryPoint
     {
-        private readonly TaskRunner taskRunner;
+        private TaskRunner taskRunner;
 
-        public LambdaEntryPoint() => this.taskRunner = new TaskRunner();
+        public async Task RunTasks(ILambdaContext context)
+        {
+            if (this.taskRunner == null)
+            {
+                this.taskRunner = await CreateTaskRunner();
+            }
 
-        public async Task RunTasks(ILambdaContext context) => await this.taskRunner.RunTasksAsync();
+            await this.taskRunner.RunTasksAsync();
+        }
+
+        private static async Task<TaskRunner> CreateTaskRunner()
+        {
+            using (var client = new AmazonSimpleSystemsManagementClient(RegionEndpoint.EUWest2))
+            {
+                var request = new GetParameterRequest {Name = "/parkingrota/ParkingRotaConnectionString"};
+                var response = await client.GetParameterAsync(request);
+
+                var connectionString = response.Parameter.Value;
+
+                return new TaskRunner(connectionString);
+            }
+        }
     }
 }
