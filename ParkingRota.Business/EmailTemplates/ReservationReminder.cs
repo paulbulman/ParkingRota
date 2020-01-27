2ce982a53371a59dcd0b79a3dc4e909086a3dadb
Dan Carter <djcarter85@gmail.com>
2020-01-27 13:04:42 +0000

    Add day to formatted date in Reservation Reminder Emails

--- ParkingRota.Business/EmailTemplates/ReservationReminder.cs
+++ ParkingRota.Business/EmailTemplates/ReservationReminder.cs
@@ -14,12 +14,12 @@
 
         public string To { get; }
 
-        public string Subject => $"No reservations entered for {this.localDate.ForDisplay()}";
+        public string Subject => $"No reservations entered for {this.localDate.ForDisplayWithDayOfWeek()}";
 
         public string HtmlBody => $"<p>{this.PlainTextBody}</p>";
 
         public string PlainTextBody =>
-            $"No reservations have yet been entered for {this.localDate.ForDisplay()}." +
+            $"No reservations have yet been entered for {this.localDate.ForDisplayWithDayOfWeek()}." +
             " If no spaces need reserving for this date then you can ignore this message." +
             " Otherwise, you should enter reservations by 11am to ensure spaces are allocated accordingly.";
     }
--- ParkingRota.UnitTests/Business/EmailTemplates/ReservationReminderTests.cs
+++ ParkingRota.UnitTests/Business/EmailTemplates/ReservationReminderTests.cs
@@ -21,17 +21,17 @@
         [Fact]
         public static void TestSubject()
         {
-            var email = new ReservationReminder(default(string), 11.December(2018));
+            var email = new ReservationReminder(default(string), 15.December(2018));
 
-            Assert.Equal("No reservations entered for 11 Dec", email.Subject);
+            Assert.Equal("No reservations entered for Sat 15 Dec", email.Subject);
         }
 
         [Fact]
         public static void TestBody()
         {
-            var email = new ReservationReminder(default(string), 1.January(2019));
+            var email = new ReservationReminder(default(string), 3.January(2019));
 
-            const string ExpectedText = "No reservations have yet been entered for 01 Jan";
+            const string ExpectedText = "No reservations have yet been entered for Thu 03 Jan";
 
             Assert.True(email.HtmlBody.Contains(ExpectedText, StringComparison.InvariantCulture));
             Assert.True(email.PlainTextBody.Contains(ExpectedText, StringComparison.InvariantCulture));
